5e1f5798cf719104b000e1cae81e5358427d7c09
Abiodun Michael <michaelmedewase@gmail.com>
2020-02-11 19:35:20 +0100

    Updates

---
+++ precache-manifest.fdd35436408179e3b79d9311a315a75a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a370428301011bd5915c1e6e0a1e8f",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "7e8f1bdc7496eeefdad5",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95b661d4d4129b38619a",
+    "url": "/hello/static/js/2.6949f162.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hello/static/js/2.6949f162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8f1bdc7496eeefdad5",
+    "url": "/hello/static/js/main.060c1434.chunk.js"
+  },
+  {
+    "revision": "de368dcbb9a951cc3091",
+    "url": "/hello/static/js/runtime-main.033cf731.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
